3446cd52116addc719a6aed30addd519075dafc3
Carlos Mourullo <carlos@frontastic.cloud>
2022-03-07 12:19:37 +0100

    chore: removed unused controller

--- commercetools/actionControllers/InvoiceController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from '@frontastic/extension-types/src/ts/index';
-import { ActionContext } from '@frontastic/extension-types';
-import { CartApi } from '../CartApi';
-import { CartFetcher } from '../../utils/CartFetcher';
-import { Payment, PaymentStatuses } from '../../../types/cart/Payment';
-
-type ActionHook = (request: Request, actionContext: ActionContext) => Promise<Response>;
